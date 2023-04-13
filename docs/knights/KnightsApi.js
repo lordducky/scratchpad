b963203b4c079906e858522a40c3412ac8513484
Xi-Lin Yeh <xyeh@nextdoor.com>
2023-04-13 14:21:19 -0700

    Add new knights package

---
+++ docs/knights/KnightsApi.js
@@ -0,0 +1,52 @@
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.KnightsApi = factory());
+})(this, (function () { 'use strict';
+
+  const validateSize = (size) => {
+    return size.length !== 2 ||
+      size[0] < 0 ||
+      size[1] < 0;
+  };
+  const generateRandomCoords = (size) => {
+    const x = Math.floor(Math.random() * size[0]);
+    const y = Math.floor(Math.random() * size[0]);
+    return [x, y];
+  };
+  const KnightsAPI = {
+    getNewKnight: (size = [8,8]) => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (!validateSize(size)) {
+            reject('Invalid size parameter');
+          }
+          resolve({
+            image: "https://lordducky.github.io/scratchpad/knights/assets/knight.svg",
+            position: generateRandomCoords(size)
+          });
+        }, 200);
+      });
+    },
+    getNewTarget: (size = [8,8]) => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (!validateSize(size)) {
+            reject({
+              status: 500,
+              success: false,
+              message: 'Invalid size parameter'
+            });
+          }
+          resolve({
+            image: "",
+            position: generateRandomCoords(size)
+          });
+        }, 200);
+      });
+    }
+  };
+
+  return KnightsAPI;
+
+}));
